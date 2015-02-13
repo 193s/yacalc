a742cc1f15e9a4862eeb1c3446d557e02ad99bd3
Ikumi Shimizu <193s@users.noreply.github.com>
2015-02-14 00:18:19 +0900

    fix a fatal bug

--- main.scala
+++ main.scala
@@ -2,15 +2,18 @@ import scala.util.parsing.combinator._
 
 test("1, 1, 0xcafe, 0xbe")
 test("0xcafebabe + 0xdeadbeef")
+test("1 + 2 + 3")
+test("1 - 1")
 
 def test(str: String): Unit =
-  Option(YACalc(str).get) match {
+  YACalc(str) match {
     case Some(result) =>
       import scala.io.AnsiColor._
       val res = result.mkString(", ")
       println(s"$GREEN$str$RESET -> $res")
     case None =>
-      println(s"$str: failed")
+      import scala.io.AnsiColor._
+      println(s"$RED$str$RESET: failed")
   }
 
 object YACalc extends RegexParsers {
@@ -25,7 +28,7 @@ object YACalc extends RegexParsers {
   def binaryNumeral: T  = "0b" ~> "[01]+".r ^^ {
     BigInt(_, 2)
   }
-  def hexNumeral: T     = "0x" ~> "[1-9a-f]+".r ^^ {
+  def hexNumeral: T     = "0x" ~> "[1-9a-fA-F]+".r ^^ {
     BigInt(_, 16)
   }
   def decimalNumeral: T = "[1-9][0-9]*".r ^^ {
@@ -36,5 +39,6 @@ object YACalc extends RegexParsers {
     t => t._1 + t._2.getOrElse(0)
   }
   def list = repsep(expr, ",")
-  def apply(str: String) = parseAll(list, str)
+  def apply(str: String) = Option(parseAll(list, str) getOrElse null)
 }
+
